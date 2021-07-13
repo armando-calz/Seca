6f6759d1efb66eac8f8a3b0ab86719dec6163de5
Armando Calzada <armcalz@outlook.com>
2021-07-13 11:24:18 -0500

    add sc.js

---
+++ sc.js
@@ -0,0 +1,7 @@
+//alert("www.seca.com quiere acceder a su ubicación, ¿desea permitir?")
+
+var usuario = document.getElementById("textoUsuario");
+var contraseña = document.getElementById("textoContraseña");
+
+console.log(usuario);
+console.log(contraseña);
