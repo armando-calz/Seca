d77df2ba15cc53ffa6e8e57e2289f7b67ffa01ac
Armando Calzada <armcalz@outlook.com>
2021-07-26 20:23:49 -0500

    attempt one vali

--- sc.js
+++ sc.js
@@ -1,7 +1,15 @@
 //alert("www.seca.com quiere acceder a su ubicación, ¿desea permitir?")
+//<a href="https://www.google.com.mx/maps/search/servicios/@19.531244,-99.266656,13.36z"target="_blank">
+var usuario = document.getElementById("textoUsuario").value;
+var contraseña = document.getElementById("textoContraseña").value;
+var passw = parseInt(contraseña.value);
+var uregist = "19"
+console.log(passw);
 
-var usuario = document.getElementById("textoUsuario");
-var contraseña = document.getElementById("textoContraseña");
-
-console.log(usuario);
-console.log(contraseña);
+function FunIniciar()
+{
+  if (passw = "19")
+  {
+    console.log("agar")
+  }
+}
