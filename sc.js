82a9c48bb650c76096cc546fac74954bef489611
Armando Calzada <armcalz@outlook.com>
2021-08-08 23:34:13 -0500

    pre presentation

--- sc.js
+++ sc.js
@@ -1,4 +1,5 @@
 swal("importante","www.seca.com quiere acceder a su ubicación, ¿desea permitir?","warning",{buttons:["no","si"]});
+//alert("www.seca.com quiere acceder a su ubicación, ¿desea permitir?")
 var usuario = document.getElementById("textoUsuario").value;
 //var contraseña = document.getElementById("textoContraseña").value;
 //var passw = parseInt(contraseña.value);
@@ -13,7 +14,9 @@ function FunIniciar()
   if(validPasswords == contraseña)
   {
     window.open("https://www.google.com.mx/maps/search/servicios/@19.531244,-99.266656,13.36z");
+    //swal("Bienvenido Acalzada","")
   }
+
   else
   {
     swal("La cotraseña es incorrecta","","error");
